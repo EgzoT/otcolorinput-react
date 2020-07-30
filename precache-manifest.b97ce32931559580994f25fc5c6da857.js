21aca07cd1d5474625871ede4ec3c6a3d9a13fc9
EgzoT <przemek7575@prokonto.pl>
2020-07-31 00:30:06 +0200

    Add gh-pages

---
+++ precache-manifest.b97ce32931559580994f25fc5c6da857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65498eada85e305431198fa0e2b68c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fec97f648ee55040a110",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "898fb0e6e1a4bb32c277",
+    "url": "/static/js/2.b8efd42a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8efd42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec97f648ee55040a110",
+    "url": "/static/js/main.128bebf8.chunk.js"
+  },
+  {
+    "revision": "2d1bc8ad81f1d4dec84b",
+    "url": "/static/js/runtime-main.e7b0ea39.js"
+  }
+]);
